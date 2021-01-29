b9ddd0b2d48813879d4bbd35b22b0555bda0ec08
Chandy Olesen <cbolesen@yahoo.com>
2021-01-29 09:32:45 -0700

    Additional formatting updates
    added lots of stuff including images and formatting

--- js/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Your index.js file is loaded correctly!");
-$(document).ready(function(){
-    $("#workGrid").hover(function(){
-        alert("Welcome to my work!");
-    },
-    function(){
-      alert("Godbye! Thank you for visiting me!");
-    }); 
-  });
